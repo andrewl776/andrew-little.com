f8e584cd237bce3e8f354f742c08fea5fa15d740
Andrew Little <alittle@ascent.io>
2021-12-22 18:08:37 +0000

    Try new GH actions

---
+++ public/posts/readmes/template.R
@@ -0,0 +1,11 @@
+write_template <- function(date) {
+
+ template <- c("---", "title: \"{FPLdata} R Package\"", "thumbnailImagePosition: left",
+    "thumbnailImage: //d1u9biwaxjngwg.cloudfront.net/cover-image-showcase/city-750.jpg",
+    "date: {{date}}", "categories:", "  - Packages", "- features",
+    "tags:", "  - pagination", "showPagination: false", "---", "",
+    "")
+
+ unname(vapply(template, function(s) glue::glue(s, .open = "{{", .close = "}}"), character(1)))
+
+}
